07b0cee7c87bf3d373224c476209745f5872290e
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-11-07 14:20:24 +0900

    vue導入

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,76 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+    <div class="signup-form">
+        <div class="signup-form-box">
+          <h2 class="signup-form-box-title">Habiter</h2>
+          <p class="signup-form-box-desc">
+            登録してMGKの写真や動画をチェックしよう
+          </p>
+          <div class="signup-form-box-form">
+            <form >
+              <div class="signup-form-box-form-facebook">
+                <a href="#">Facebookでログイン</a>
+              </div> 
+              <div class="signup-form-box-form-another">
+                <div></div>
+                <p>または</p>
+                <div></div>
+              </div>  
+              <div class="form-error" v-if="errors.length != 0">
+                <ul v-for="e in errors" :key="e">
+                  <li><font color="red">{{ e }}</font></li>
+                </ul>
+              </div>
+              <div class="signup-form-box-form-email">
+                <input type="email" v-model="email" placeholder="携帯電話番号またはメールアドレス" >
+              </div>
+              <div class="signup-form-box-form-fullname">
+                <input type="text" v-model="nickname" placeholder="ニックネーム" >
+              </div>
+              <div class="signup-form-box-form-password">
+                <input type="password" v-model="password" placeholder="パスワード" >
+              </div>
+              <div class="signup-form-box-form-submit">
+                <button @click="setAxiosDefaults" type="submit">登録する</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      message: "Hello Vue!"
+import axios from 'axios';
+export default{
+  data(){
+    return{
+        email:"",
+        nickname:"",
+        password:"",
+        errors:[],
+    }
+  },
+  methods:{ 
+    submit: function() {
+       this.errors=[]
+    },
+    getCsrfToken: function(){
+      if (!(axios.defaults.headers.common['X-CSRF-Token'])) {
+        return (
+          document.getElementsByName('csrf-token')[0].getAttribute('content')
+        )
+        } 
+      else {
+        return (  
+          axios.defaults.headers.common['X-CSRF-Token']
+        )
+      }
+    },
+    setAxiosDefaults: function(){
+      axios.defaults.headers.common['X-CSRF-Token'] = this.getCsrfToken();
+      axios.defaults.headers.common['Accept'] = 'application/json';
+      console.log(axios.defaults.headers.common['X-CSRF-Token']);
     }
   }
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
