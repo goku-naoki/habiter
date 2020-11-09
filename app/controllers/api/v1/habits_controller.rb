8146a7777a4768f6ac9aed9cfa749dc16187d327
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-11-09 16:52:56 +0900

    index のapi

--- app/controllers/api/v1/habits_controller.rb
+++ app/controllers/api/v1/habits_controller.rb
@@ -5,6 +5,11 @@ class Api::V1::HabitsController < ApiController
     render json: { error: '404 not found' }, status: 404
   end
 
+  def index
+    habits=Habit.where(user_id:current_user.id)
+    render json: habits
+  end
+
   def create
     @habit=Habit.new(habit_params)
     if @habit.save
--- app/javascript/components/habits/HabitIndex.vue
+++ app/javascript/components/habits/HabitIndex.vue
@@ -11,6 +11,20 @@ import IndexHeader from './IndexHeader'
 import HabitList from './HabitList'
 
 export default{
+
+  data(){
+    return{
+
+    }
+  },
+  created(){
+    axios
+      .get('/api/v1/posts.json')
+      .then(response => {
+        this.posts = response.data
+        this.isLoading=false
+      })
+  },
   components:{
     IndexHeader,
     HabitList
