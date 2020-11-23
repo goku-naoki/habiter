5a3957cb26a36c5610125768c23663673c6442c9
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-11-23 11:52:06 +0900

    Update HabitForm.vue

--- app/javascript/components/habits/HabitForm.vue
+++ app/javascript/components/habits/HabitForm.vue
@@ -48,6 +48,7 @@ export default{
   },
   methods:{
     submit:function(event){
+      event.preventDefault()
       const habit={name:this.name,date:this.date}
       this.$emit('submit',habit)
     },
