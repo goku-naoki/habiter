0c76ac4a63c6dde6d8274ec4fc639e4b9caaa1ad
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-12-02 14:58:59 +0900

    切り替え

---
+++ app/javascript/components/user/MyFollowed.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    fofe
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios';
+import MyHabit from './MyHabit'
+export default{
+
+ 
+  
+
+}
+
+</script>
+
+<style scoped lang="scss">
+  
+
+</style>
--- app/javascript/components/user/MyHabits.vue
+++ app/javascript/components/user/MyHabits.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="my-habits">
     <div class="my-habits__inner">
-      <p class="my-habits__inner-title">
-        Habits
-      </p>
-
-      <!-- 2つ条件入れて置かないと、habit_user取れていないときにエラー -->
-      <p v-if="!user.habit_users ||user.habit_users.length==0" class="my-habits__inner-no">習慣が登録されていません</p>
-      <ul v-else class="my-habits__inner__list">
-        <MyHabit :habit-user="habitUser" v-for="habitUser in user.habit_users" :key="habitUser.id"/>
-      </ul>
+      <div class="my-habits__inner-titles">
+        <p class="my-habits__inner-title" @click="touchHabit()">
+          Habits
+        </p>
+        <p class="my-habits__inner-title" @click="touchFollow()">
+          Following
+        </p>
+      </div>
+      
+      <template v-if="isHabitActive">
+        <!-- 2つ条件入れて置かないと、habit_user取れていないときにエラー -->
+        <p v-if="!user.habit_users ||user.habit_users.length==0" class="my-habits__inner-no">習慣が登録されていません</p>
+        <ul v-else class="my-habits__inner__list">
+          <MyHabit :habit-user="habitUser" v-for="habitUser in user.habit_users" :key="habitUser.id"/>
+        </ul>
+      </template>
+      <template v-else>
+        fff
+      </template>
+      
      
     </div>
   </div>
@@ -19,11 +30,26 @@
 <script>
 import axios from 'axios';
 import MyHabit from './MyHabit'
+import MyFollowed from './MyFollowed'
 export default{
-
+  
+   data(){
+     return{
+       isHabitActive:true,
+       isFollowActive:false
+     }
+   },
    props:{
     user:{
       habit_users:Array
+    }
+  },
+  methods:{
+    touchHabit(){
+     this.isHabitActive==false ? this.isHabitActive=true  :{}
+    },
+    touchFollow(){
+     this.isHabitActive==true ? this.isHabitActive=false  :{}
     }
   },
   
@@ -37,7 +63,8 @@ export default{
   },
 
 components:{
-  MyHabit
+  MyHabit,
+  MyFollowed
 }
 
   
@@ -57,10 +84,17 @@ components:{
       width:95%;
       height: 100%;;
       margin:0 auto;
+     
       padding-top:40px;
-      &-title{
-        font-size:1.8rem;
+      &-titles{
         margin-bottom:20px;
+        display:flex;
+        p{
+          font-size:1.8rem;
+          &:first-child{
+            margin-right:30px;
+          }
+        }
       }
       &__list{
         display: flex;
