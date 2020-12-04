f05b3cc96d0fdf34e8a88ca1a4317e65de039d59
goku-naoki <naoki.mangoku@di-v.co.jp>
2020-12-04 10:59:03 +0900

    gem rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  
+  validates :nickname, presence: true
+
   has_many :habit_users
   has_many :habits, through: :habit_users
 
